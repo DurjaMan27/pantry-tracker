32a253953efe45a6b889bf63d20843d4d68e810f
Varun Rao <varunvellorerao@gmail.com>
2024-08-03 14:28:59 -0400

    Fixing errors in firestore

--- inventory-management-app/src/firebase.js
+++ inventory-management-app/src/firebase.js
@@ -8,15 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC_ZYvTDc2cLX1FHt3EZ8c1HSK3M0tAGbQ",
-  authDomain: "pantry-tracker-83d2f.firebaseapp.com",
-  projectId: "pantry-tracker-83d2f",
-  storageBucket: "pantry-tracker-83d2f.appspot.com",
-  messagingSenderId: "747422121973",
-  appId: "1:747422121973:web:e8bdb51abb60c16811aabc",
-  measurementId: "G-5D4SKYD623"
+    apiKey: "AIzaSyC_ZYvTDc2cLX1FHt3EZ8c1HSK3M0tAGbQ",
+    authDomain: "pantry-tracker-83d2f.firebaseapp.com",
+    projectId: "pantry-tracker-83d2f",
+    storageBucket: "pantry-tracker-83d2f.appspot.com",
+    messagingSenderId: "747422121973",
+    appId: "1:747422121973:web:e8bdb51abb60c16811aabc",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
